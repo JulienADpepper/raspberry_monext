6e1b0402a8b5c0d655b74788bf84c7b5698ff088
Julien AIME <jaime@monext.net>
2022-06-02 14:24:01 +0200

    version avec cycle de vie est point

--- code_princ.py
+++ code_princ.py
@@ -21,8 +21,6 @@ LED = int(config['Port_de_sortie']['led']) #Définit le numéro du port GPIO qui
 
 GPIO.setup(LED, GPIO.OUT) #Active le contrôle du GPIO
 
-state = GPIO.input(LED) #Lit l'état actuel du GPIO, vrai si allumé, faux si éteint
-
 url = config['Curl']['url'] #définition de l'url de l'api pour extraire les informations 
 
 type_header = config['Curl']['type_header'] #définition des variables du headers
@@ -39,29 +37,30 @@ with open ('liste', 'rb') as f1:
 
 #definition des fonctions
 
-def liste_des_erreurs(l) : #permet de ne récupérer que les cas qui nous interresse 
+def liste_des_erreurs(listep) : #permet de ne récupérer que les cas qui nous interresse 
     result=[] #après la requette cURL
     type_erreur = config['Filtre']['type_erreur'] #recupere du fichier de conf les bonnes valeurs
     severite = int(config['Filtre']['niveau_de_severite']) #récupere le niveau de severite du fichier de conf 
-    for i in l :
+    for i in listep :
         if (i['type']==type_erreur) and (i['severity']==severite) and (i['eventId'] not in OL):
             result.append(i) #result récupere les elements de l ayant le bon type d'erreur et la bonne severite
     return result
 
-def allumage(l): #programme chargé de l'allumage de la led sur le raspberry
+def allumage(listep): #programme chargé de l'allumage de la led sur le raspberry
     logging.info('entré dans l allumage')
-    if (len(l) != 0) and not(state) : #si la liste n'est pas vide et la LED est éteinte alors il y a des alertes critical donc on allume
+    state = GPIO.input(LED) #Lit l'état actuel du GPIO, vrai si allumé, faux si éteint
+    if (len(listep) != 0) and not(state) : #si la liste n'est pas vide et la LED est éteinte alors il y a des alertes critical donc on allume
         logging.info('led allumé')
         GPIO.output(LED, GPIO.HIGH) #allumage de la LED
         return True #utile pour verifier le bon fonctionnement du programme
-    if (len(l) == 0) and state :#si la liste est vide et la LED est allumé alors il n'y a plus d'alertes critical et donc on allume
+    if (len(listep) == 0) and state :#si la liste est vide et la LED est allumé alors il n'y a plus d'alertes critical et donc on allume
         logging.info('led éteinte')
         GPIO.output(LED, GPIO.LOW)#extinction de la LED
         return False #utile pour verifier le bon fonctionnement du programme
 
-def acquittement(l): #demande a l'utilisateur s'il veut acquitter les evennements et enrichie la liste des evenemments acquittés
+def acquittement(listep): #demande a l'utilisateur s'il veut acquitter les evennements et enrichie la liste des evenemments acquittés
     logging.info('entré dans l acquittement')
-    for i in l :
+    for i in listep :
         question = "voulez-vous acquiter l'event : ",  i['eventId'], "?"
         resp = input (question)
         if resp == 'oui' :
@@ -74,5 +73,4 @@ def acquittement(l): #demande a l'utilisateur s'il veut acquitter les evennement
 
 #affichage
 allumage(liste_des_erreurs(l)) #lancement du code, mettre print devant pour verifier les sortie true et false
-acquittement(liste_des_erreurs(l)) #lance la procédure d'acquittement
-
+print(acquittement(liste_des_erreurs(l))) #lance la procédure d'acquittement
